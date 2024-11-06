6ee1b18f9c11dd670b50e56a414eec4e3edcdb4e
Endekalu Simon <endekalu.simon.haile@gmail.com>
2024-11-06 21:12:24 +0300

    feat: Set up logging configuration and directory management
    - Implemented functionality to create a `logs` directory if it does not already exist, ensuring proper logging structure.
    - Configured the logging system to capture log messages with a timestamped filename for better traceability.
    - Set the logging level to INFO and defined a consistent message format for clarity in log output.

---
+++ scripts/data_handler.py
@@ -0,0 +1,19 @@
+import os
+import logging
+from datetime import datetime
+
+# Create log folder if it doesnot exist
+log_directory = "../logs"
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
+
+# Configure logging
+log_filename = os.path.join(log_directory, f'historical_data_{datetime.now().strftime("%Y%m%d_%H%M%S")}.log')
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler(log_filename)
+    ]
+)
